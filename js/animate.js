4b8d039d1eb47e30d93e797008363b8428be6fff
Rojan Maharjan <rojanmhj@gmail.com>
2023-03-19 17:49:10 -0700

    Added certification section

--- js/animate.js
+++ js/animate.js
@@ -4,7 +4,7 @@ function reveal() {
     for (var i = 0; i < reveals.length; i++) {
       var windowHeight = window.innerHeight;
       var elementTop = reveals[i].getBoundingClientRect().top;
-      var elementVisible = 190;
+      var elementVisible = 150;
   
       if (elementTop < windowHeight - elementVisible) {
         reveals[i].classList.add("active");
