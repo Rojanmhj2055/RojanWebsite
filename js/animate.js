dc800f0033448ddf95e8c36659eb0d5b1ffafc12
Rojan Maharjan <rojanmhj@gmail.com>
2023-03-30 13:25:21 -0700

    Day/Night mode

--- js/animate.js
+++ js/animate.js
@@ -13,5 +13,32 @@ function reveal() {
       }
     }
   }
+
+
+function nightMode(){
+
+let r = document.querySelector(':root');
+let p = document.getElementById('nightmode');
+let j = document.getElementById('nighticon');
+if(p.classList[0] =='day'){
+  r.style.setProperty('--text-color', 'white');
+  r.style.setProperty('--background-color', 'black');
+  r.style.setProperty('--div-background-color','black');
+  j.classList.remove("fa-moon");
+  j.classList.add("fa-sun");
+  p.classList.remove("day");
+  p.classList.add("night");
+}else if(p.classList[0]=='night'){
+  r.style.setProperty('--text-color', 'black');
+  r.style.setProperty('--background-color', '#f7fafc');
+  r.style.setProperty('--div-background-color','#ffffff');
+  j.classList.remove("fa-sun");
+  j.classList.add("fa-moon");
+  p.classList.remove("night");
+  p.classList.add("day");
+}
+
+
+}
   
   window.addEventListener("scroll", reveal);
